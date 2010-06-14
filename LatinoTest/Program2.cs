9074c3b149ca72a781764db1ad97d0a218e323c4
mgrcar <mgrcar@35089d11-96e9-ae4d-82a0-4b2588c93174>
2010-06-14 10:58:59 +0000

    git-svn-id: svn://first.ijs.si:3691/latino@60 35089d11-96e9-ae4d-82a0-4b2588c93174

--- LatinoTest/Program2.cs
+++ LatinoTest/Program2.cs
@@ -10,26 +10,28 @@ namespace LatinoTest
     {
         static void Main(string[] args)
         {
-            LanguageDetector langDet = new LanguageDetector(2);
-            langDet.ReadCorpus(@"C:\Users\Miha\Desktop\LangDetectCorpus");
-            //LanguageProfile p = langDet.FindMatchingLanguage("To je slovenski stavek. Čeprav ga naš detektor ne zazna pravilno. Mogoče šumniki pomagajo...");
-            //Console.WriteLine(p.Code);
+            LanguageDetector langDet = new LanguageDetector();
+            langDet.ReadCorpus(@"C:\Users\mIHA\Desktop\langdet\MultextEast");
+            LanguageProfile p = langDet.FindMatchingLanguage("To je slovenski stavek. Čeprav ga naš detektor ne zazna pravilno. Mogoče šumniki pomagajo...");
+            Console.WriteLine(p.Language);
+            p = langDet.FindMatchingLanguage("Funny thing... This is a very short English sentence...");
+            Console.WriteLine(p.Language);
             //Console.WriteLine(langDet.GetLanguageProfile("et"));
-            StreamWriter w = new StreamWriter("c:\\krneki\\langSim.txt");
-            foreach (LanguageProfile p in langDet.LanguageProfiles)
-            {
-                w.Write("{0}\t", p.Code);
-            }
-            w.WriteLine();
-            foreach (LanguageProfile p in langDet.LanguageProfiles)
-            {
-                foreach (LanguageProfile p2 in langDet.LanguageProfiles)
-                {
-                    //w.Write("{0}\t", Math.Max(p.CalcSpearman(p2), p2.CalcSpearman(p)));
-                }
-                w.WriteLine();
-            }
-            w.Close();
+            //StreamWriter w = new StreamWriter("c:\\krneki\\langSim.txt");
+            //foreach (LanguageProfile p in langDet.LanguageProfiles)
+            //{
+            //    w.Write("{0}\t", p.Code);
+            //}
+            //w.WriteLine();
+            //foreach (LanguageProfile p in langDet.LanguageProfiles)
+            //{
+            //    foreach (LanguageProfile p2 in langDet.LanguageProfiles)
+            //    {
+            //        //w.Write("{0}\t", Math.Max(p.CalcSpearman(p2), p2.CalcSpearman(p)));
+            //    }
+            //    w.WriteLine();
+            //}
+            //w.Close();
         }
     }
 }
