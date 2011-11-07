1c2b9b3149cb75cebae416253109d11cad5d1a4c
Jernej Virag <jernej.virag@gmail.com>
2011-11-07 15:52:05 +0100

    Cut down on needless allocation count when calculating dot product

--- Latino/Model/DotProductSimilarity.cs
+++ Latino/Model/DotProductSimilarity.cs
@@ -42,13 +42,6 @@ namespace Latino.Model
 
         public double GetSimilarity(SparseVector<double> a, SparseVector<double> b)
         {
-            return GetSimilarity(new SparseVector<double>.ReadOnly(a), new SparseVector<double>.ReadOnly(b));
-        }
-
-        // *** ISimilarity<SparseVector<double>.ReadOnly> interface implementation ***
-
-        public double GetSimilarity(SparseVector<double>.ReadOnly a, SparseVector<double>.ReadOnly b)
-        {
             Utils.ThrowException(a == null ? new ArgumentNullException("a") : null);
             Utils.ThrowException(b == null ? new ArgumentNullException("b") : null);
             double dotProd = 0;
@@ -56,10 +49,10 @@ namespace Latino.Model
             int aCount = a.Count;
             int bCount = b.Count;
             if (aCount == 0 || bCount == 0) { return 0; }
-            ArrayList<int> aIdx = a.Inner.InnerIdx;
-            ArrayList<double> aDat = a.Inner.InnerDat;
-            ArrayList<int> bIdx = b.Inner.InnerIdx;
-            ArrayList<double> bDat = b.Inner.InnerDat;            
+            ArrayList<int> aIdx = a.InnerIdx;
+            ArrayList<double> aDat = a.InnerDat;
+            ArrayList<int> bIdx = b.InnerIdx;
+            ArrayList<double> bDat = b.InnerDat;
             int aIdx_i = aIdx[0];
             int bIdx_j = bIdx[0];
             while (true)
@@ -79,10 +72,16 @@ namespace Latino.Model
                     dotProd += aDat[i] * bDat[j];
                     if (++i == aCount || ++j == bCount) { break; }
                     aIdx_i = aIdx[i];
-                    bIdx_j = bIdx[j];                    
+                    bIdx_j = bIdx[j];
                 }
             }
             return dotProd;
+        }
+
+        // *** ISimilarity<SparseVector<double>.ReadOnly> interface implementation ***
+        public double GetSimilarity(SparseVector<double>.ReadOnly a, SparseVector<double>.ReadOnly b)
+        {
+            return GetSimilarity(a.Inner, b.Inner);
         }
 
         // *** ISerializable interface implementation ***
